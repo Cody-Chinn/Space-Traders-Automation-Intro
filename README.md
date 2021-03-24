bcf8cf57ff2cfd41f54a8673d9a9ba47363c3089
Cody Chinn <cdchinn14@gmail.com>
2021-03-23 21:34:41 -0400

    updating readme and adding error checking to index start script

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # Space-Traders-Automation-Intro
 An introduction to automation looping in the game Space Traders
+
+## Introduction
+Space traders is a backend to a trading game in space. The front end was intentionally left out so people could learn how to interact with RESTful API's. There are already a ton of awesome projects being created for this game and can be found in the [community discord](https://discord.gg/tQcRvx6a). This repo is dedicated to the beginners that want to learn how to create automation loops to really max out your credits.
+
+# Getting started
+ - Create a directory to keep this code in
+ - Open the cmd line or terminal and navigate to the newly created folder
+ - run ```git clone https://github.com/Cody-Chinn/Space-Traders-Automation-Intro.git```
+ - open your editor (I use [VS Code](https://code.visualstudio.com/download))
+ - Edit the index file a desired username
+ - Make sure you have [Node JS](https://nodejs.org/en/download/) installed and then run npm install from your cmd line or terminal
+    - tip for VS Code users: the keybind commands ``` control + ` ``` to open the terminal
+ - Once all of the packages are installed, run ```npm start```
+ - That's it! All of the important data should now be outputting to the terminal 😃
+
+#Todo:
+## Testing in the API
+- [ ] flightplans
+    - [ ] getFlightPlansInSystem
+    - [ ] getFlightPlanById
+    - [x] submitNewFlightPlan
+- [ ] game
+    - [ ] getGameStatus
+- [ ] loans
+    - [ ] getAvailableLoans
+    - [ ] getCurrentLoans
+    - [ ] payoffLoan
+    - [x] requestNewLoan
+- [ ] locations
+    - [ ] getLocationInfo
+    - [ ] getDockedShipsInfo
+    - [ ] getLocationsInSystem
+- [ ] marketplace
+    - [ ] getMarketInfo
+- [x] purchase orders
+    - [x] placePurchaseOrder
+- [x] sell orders
+    - [x] sellGoods
+- [ ] ships
+    - [x] buyShip
+    - [ ] getAvailableShips
+    - [ ] getShipInfoById
+    - [ ] getPlayersShips
+    - [ ] jettisonCargo
+    - [ ] scrapShip
+- [ ] systems
+    - [ ] getSystemInfo
+- [x] users
+    - [x] createNewUser
+    - [x] getUserInfo
+
+## Index changes
+- [x] Add error checking for each call
+- [ ] Add a loop to go back and forth to each planet
--- index.js
+++ index.js
@@ -10,20 +10,30 @@ async function startScript(){
     // Each call made to the api has a flow of index.js -> Api/{endpoint}.js -> apiCall.js
     // This is to cut down on the amount of copy/pasted code in each file so each file used 
     // in the index will need to be required at the top in order to use the functions
-    const userTest = await users.createNewUser('TestSpaceScorpion20');
-    
-    // Lets get make sure the username isn't taken first
-    if(!userTest.user.username){
-        console.log('Looks like that username is taken :( ');
+    const userTest = await users.createNewUser('TestSpaceScorpion22');
+
+    // Lets get make sure the username isn't taken first and display an error message if 
+    // there was one present on the returned json. These checks are placed throughout the
+    // loop to make sure each call works as expected
+    if(userTest.error){
+        console.log('ERROR CREATING USER: ');
+        console.log('---------------------');
+        console.log(userTest.error.message);
         return;
     }
 
     // Kickoff the automation and notify the user of their token
-    console.log(`Hello ${userTest.user.username}, beginning your automation now!`)
-    console.log(`You'll want to save this token. It's pretty important ${userTest.token}`);
+    console.log(`Hello ${userTest.user.username}, beginning your automation now!`);
+    console.log(`You'll want to save this token. It's pretty important --> ${userTest.token}`);
     
     // Take out a loan so we can purchase a ship
-    await loans.requestNewLoan(userTest.user.username, userTest.token, 'STARTUP');
+    const loan = await loans.requestNewLoan(userTest.user.username, userTest.token, 'STARTUP');
+    if(loan.error){
+        console.log('ERROR TAKING OUT LOAN: ');
+        console.log('-----------------------');
+        console.log(loan.error.message);
+        return;
+    }
     
     console.log(`Loan recieved, taking 1.5 second nap`);
 
@@ -33,25 +43,55 @@ async function startScript(){
     
     // Buy a ship and grab the Id for future functions
     const meNewShip = await ships.buyShip(userTest.user.username, userTest.token, 'OE-PM-TR', 'JW-MK-I');
+    if(meNewShip.error){
+        console.log('ERROR BUYING SHIP: ');
+        console.log('-------------------');
+        console.log(meNewShip.error.message);
+        return;
+    }
     const meNewShipId = meNewShip.user.ships[0].id;
     
     // Purchase Fuel for the newly aquired ship
-    await purchaseOrder.placePurchaseOrder(userTest.user.username, userTest.token, meNewShipId, 'FUEL', 20);
+    const fuelOrder = await purchaseOrder.placePurchaseOrder(userTest.user.username, userTest.token, meNewShipId, 'FUEL', 20);
+    if(fuelOrder.error){
+        console.log('ERROR BUYING FUEL: ');
+        console.log('-------------------');
+        console.log(fuelOrder.error.message);
+        return;
+    }
     console.log(`Fueled up, lets get some cargo after a 1.5 second nap`);
     await sleep(1500);
 
     // Buy some METAL goods so we can sell it on another planet
-    await purchaseOrder.placePurchaseOrder(userTest.user.username, userTest.token, meNewShipId, 'METALS', 80);
+    const metalOrder = await purchaseOrder.placePurchaseOrder(userTest.user.username, userTest.token, meNewShipId, 'METALS', 80);
+    if(metalOrder.error){
+        console.log('ERROR BUYING METAL: ');
+        console.log('--------------------');
+        console.log(metalOrder.error.message);
+        return;
+    }
 
     // Send the ship to the nearest planet (in this case OE-PM since we're on OE-PM-TR)
     // To get more info on locations you can use the functions in the locations file 
     const flightplan = await flightPlans.submitNewFlightPlan(userTest.user.username, userTest.token, meNewShipId, 'OE-PM');
-    console.log(flightplan);
+    if(flightplan.error){
+        console.log('ERROR SUBMITTING FLIGHT PLAN: ');
+        console.log('------------------------------');
+        console.log(flightplan.error.message);
+        return;
+    }
+
     console.log(`Ship has liftoff, waiting 90 seconds until touchdown`);
     await sleep(90000);
 
     // Sell all of the METAL material on your ship for profit
-    await sellOrders.sellGoods(userTest.user.username, userTest.token, meNewShipId, 'METALS', 80);
+    const sellOrder = await sellOrders.sellGoods(userTest.user.username, userTest.token, meNewShipId, 'METALS', 80);
+    if(sellOrder.error){
+        console.log('ERROR SELLING METAL: ');
+        console.log('---------------------');
+        console.log(sellOrder.error.message);
+        return;
+    }
     
     // As a nice feature, there's an output of anything that could be considered useful
     // to the person running the script. None of the lines below are part of the automation
@@ -59,6 +99,12 @@ async function startScript(){
     console.log(`Tutorial automation finished. \nHere is some useful data to continue using this account.\n-------------------------------------------------------------------------------------------`);
 
     const userData = await users.getUserInfo(userTest.user.username, userTest.token); 
+    if(userData.error){
+        console.log('ERROR GETTING PLAYER DATA: ');
+        console.log('---------------------------');
+        console.log(userData.error.message);
+        return;
+    }
     console.log('User information');
     console.log(userData);
 
